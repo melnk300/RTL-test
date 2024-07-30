8bdd1e855b2171d8d8668a1c32eaf0cfbe5b92d1
Руслан Мельник <melnk300@gmail.com>
2024-07-30 22:12:39 +0500

    Design page

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,221 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="page">
+    <aside>
+      <img src="https://www.mmppicture.co.in/wp-content/uploads/2020/07/Blur-Background-64.jpg" alt="">
+      <div class="item-detail">
+        <p class="item-name skeleton"></p>
+        <div class="item-desc">
+          <p class="skeleton"></p>
+          <p class="skeleton"></p>
+          <p class="skeleton"></p>
+          <p class="skeleton"></p>
+          <p class="skeleton"></p>
+        </div>
+      </div>
+      <div class="item-meta">
+        <p class="skeleton"></p>
+      </div>
+    </aside>
+    <main>
+      <div class="item">
+        <div class="item-content"></div>
+        <div class="item-count">2</div>
+      </div>
+      <div class="item">
+        <div class="item-content"></div>
+        <div class="item-count">2</div>
+      </div>
+      <div class="item">
+        <div class="item-content"></div>
+        <div class="item-count">2</div>
+      </div>
+    </main>
+    <footer>
+      <p class="skeleton"></p>
+    </footer>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+aside {
+  grid-area: aside;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+main {
+  grid-area: main;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+footer {
+  grid-area: footer;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.page {
+  min-height: 100vh;
+  padding: 2rem;
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  background-color: #1d1d1d;
+
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-template-areas:
+    "aside main"
+    "footer footer"
+  ;
+  gap: 1.5rem;
+  width: 100%;
+}
+
+
+
+aside, main, footer {
+  background: #262626;
+  border: 1px solid #4d4d4d;
+  border-radius: .8em;
+}
+
+aside {
+  padding: 0.87rem;
+}
+
+aside > img {
+  width: 100%;
+  object-fit: cover;
+  border-radius: .5rem;
+  margin-bottom: 1.25rem;
+}
+
+.item-detail, .item-desc, .item-meta {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  flex-direction: column;
+}
+
+.item-detail > p, .item-desc > p, .item-meta > p {
+  padding: 10px;
+
+  border-radius: .5em;
+}
+
+.skeleton {
+  content: " ";
+  background: linear-gradient(90deg, #333333 25%, #636363 50%, #333333 75%);
+  background-size: 200% 100%;
+  animation: loading 1.5s infinite;
+}
+
+@keyframes loading {
+  0% {
+    background-position: 200% 0;
   }
+  100% {
+    background-position: -200% 0;
+  }
+}
+
+.item-name {
+  width: 70%;
+  height: 2em;
+  margin-bottom: 1.5rem;
+}
+
+.item-desc > p:not(:last-child) {
+  margin-bottom: 1rem;
+  width: 100%;
+}
+
+.item-desc > p:last-child {
+  margin-bottom: 1.5rem;
+  width: 100%;
+}
+
+.item-desc > p:nth-child(1) {
+  width: 75%;
+}
+
+.item-desc > p:nth-child(2) {
+  width: 90%;
+}
+
+.item-desc > p:nth-child(3) {
+  width: 80%;
+}
+
+.item-desc > p:nth-child(4) {
+  width: 78%;
+}
+
+.item-desc > p:nth-child(5) {
+  width: 75%;
+}
+
+.item-meta > p {
+  width: 50%;
+  margin-bottom: .6rem;
+}
+
+
+
+main {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(5, 1fr);
+}
+
+.item {
+  position: relative;
+  padding: 3.5em;
+  border: 1px solid #4d4d4d;
+}
+
+.item-content {
+  position: relative;
+  content: " ";
+  background-color: rgba(51, 255, 233, 1);
+  width: 100%;
+  height: 100%;
+}
+
+.item-content:after {
+  position: absolute;
+  content: " ";
+  background-color: rgba(1, 188, 169, 0.6);
+  width: 100%;
+  height: 100%;
+  bottom: 10px;
+  left: 10px;
+  backdrop-filter: blur(5px);
+}
+
+.item-count {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  padding: 10px 15px;
+  border-radius: 10px 0 0 0;
+  color: #f6f6f6;
+  border-top: 1px solid #f6f6f6;
+  border-left: 1px solid #f6f6f6;
+}
+
+
+
+
+
+
+
+footer {
+  display: flex;
+  align-items: center;
+  padding: 1.125rem;
+}
+
+footer > p {
+  width: 100%;
+  height: 2.25rem;
+  border-radius: .75rem;
 }
 </style>
